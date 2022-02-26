07f162409de93a7b9d51390ccf8defa22a198ee6
msbeigiai <msbeigi83@gmail.com>
2022-02-26 17:25:56 +0330

    Error handeling for some values added

--- make_sql_connection.py
+++ make_sql_connection.py
@@ -1,5 +1,6 @@
 import pyodbc
 
+
 class MakeSqlConnection:
     def __init__(self, database_name, table_name, username, password, server='tcp:localhost,1433'):
         self.command = None
@@ -18,10 +19,14 @@ class MakeSqlConnection:
         self.cnxn = pyodbc.connect(
             'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database_name + ';UID=' + self.username + ';PWD=' + self.password
         )
+        if self.cnxn is None:
+            raise ValueError(f"Connection for {self.database_name} is not established.")
 
     def __execution(self):
         self.__make_connection()
         self.cursor = self.cnxn.cursor()
+        if self.cursor is None:
+            raise ValueError(f"Cursor is None")
 
     def execution_command(self, command, params=None, commit=False, command_type=None):
         self.command = command
@@ -40,6 +45,8 @@ class MakeSqlConnection:
 
         if command_type == 'select':
             value = self.cursor.fetchone()
+            if value is None:
+                raise ValueError(f"Vale {self.table_name} is not exist.")
             return value[0]
 
     def __check_connection(self):
